d2ebf7edd8e5db60dbe744a2f95039cf9ee02739
suzan-amer <amersuzan579@gmail.com>
2021-04-26 20:27:02 +0300

    lab02

---
+++ js/app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+let userName = prompt('can you enter your name ,plz?');
+console.log ('username : ' +userName);
+alert ('hallo ' + userName +' welcome to my page , have fun ');
+
+
+
+let s1=prompt ('do you think i like drawing ?');
+switch(s1.toUpperCase()){
+    case 'YES' :console.log ('yes , you do');
+    case 'Y':console.log ('yes , you do');
+    break;
+    alert('yes , I do');
+    case'NO':console.log('no , you dont');
+    case 'N':console.log ('no , you dont');
+    break;
+    alert ('no ,I dont');
+    default: 
+      console.log('plz ask me agin');
+      alert('plz ask me agin');
+      
+}
+
+
+let s2=prompt ('do I speak spanish ?');
+switch(s2.toUpperCase()){
+    case 'YES' :console.log ('yes , you do');
+    case 'Y':console.log ('yes , you do');
+    break;
+    alert('yes , I do');
+    case'NO':console.log('no , you dont');
+    case 'N':console.log ('no , you dont');
+    break;
+    alert ('no ,I dont');
+    default: 
+      console.log('plz ask me agin');
+      alert('plz ask me agin');
+      
+}
+
+
+let s3=prompt ('AM I traind as carpentry?');
+switch(s3.toUpperCase()){
+    case 'YES' :console.log ('yes , you do');
+    case 'Y':console.log ('yes , you do');
+    break;
+    alert('yes , I do');
+    case'NO':console.log('no , you dont');
+    case 'N':console.log ('no , you dont');
+    break;
+    alert ('no ,I dont');
+    default: 
+      console.log('plz ask me agin');
+      alert('plz ask me agin');
+      
+}
+
+
+let s4=prompt ('AM I good at crafts ?');
+switch(s4.toUpperCase()){
+    case 'YES' :console.log ('yes , you do');
+    case 'Y':console.log ('yes , you do');
+    break;
+    alert('yes , I do');
+    case'NO':console.log('no , you dont');
+    case 'N':console.log ('no , you dont');
+    break;
+    alert ('no ,I dont');
+    default: 
+      console.log('plz ask me agin');
+      alert('plz ask me agin');
+      
+}
+
+
+let s5=prompt ('did i study at aabu unversity ?');
+switch(s5.toUpperCase()){
+    case 'YES' :console.log ('yes , you do');
+    case 'Y':console.log ('yes , you do');
+    break;
+    alert('yes , I do');
+    case'NO':console.log('no , you dont');
+    case 'N':console.log ('no , you dont');
+    break;
+    alert ('no ,I dont');
+    default: 
+      console.log('plz ask me agin');
+      alert('plz ask me agin');
+      
+}
