291c523f054e29d8d816814a1229400b0cc724d0
suzan-amer <amersuzan579@gmail.com>
2021-04-28 17:57:07 +0300

    edit 1

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # class2-201
-<<<<<<< HEAD
-sam
-=======
-### add 2q like guessing game just 
-## i'm L
- i cant write code in js 
+ 
 
  ## navigator name : suzan Amer 
  ## driver name : Dunia alkilany
  ### we work together  , the navigator suzan indicated to me to add function for each question and call it ,
  ### and the driver Dunia write the code of the function . 
->>>>>>> 2d45c854b6a1847d4764f80c9ee25938de9f50c0
+
--- js/app.js
+++ js/app.js
@@ -1,228 +1,177 @@
-// 'use strict';
+'use strict';
 
-<<<<<<< HEAD
-// let score= 0; 
+ let score= 0; 
 
-// let userName = prompt('can you enter your name ,plz?');
-// console.log ('username : ' +userName);
-// alert ('hallo ' + userName +' welcome to my page , have fun ');
-
-
-
-// let s1=prompt ('do you think i like drawing ?');
-// switch(s1.toUpperCase()){
-//     case 'YES' 
-//        :console.log ('yes , you do');
-//     case 'Y'
-//       :console.log ('yes , you do');
-//     alert('yes , I do');
-//     score++;
-//     break;
-=======
- let userName = prompt('can you enter your name ,plz?');
+let userName = prompt('can you enter your name ,plz?');
 console.log ('username : ' +userName);
 alert ('hallo ' + userName +' welcome to my page , have fun ');
 
-function question1()
+function question1 (){
 
-{let s1=prompt ('do you think i like drawing ?');
+let s1=prompt ('do you think i like drawing ?');
 switch(s1.toUpperCase()){
-    case 'YES' :console.log ('yes , you do');
-    case 'Y':console.log ('yes , you do');
+    case 'YES' 
+       :console.log ('yes , you do');
+    case 'Y'
+      :console.log ('yes , you do');
     alert('yes , I do');
+    score++;
     break;
->>>>>>> 2d45c854b6a1847d4764f80c9ee25938de9f50c0
     
-//     case'NO'
-//        :console.log('no , you dont');
-//     case 'N'
-//        :console.log ('no , you dont');
-//     alert ('no ,I dont');
-//     break;
+    case'NO'
+       :console.log('no , you dont');
+    case 'N'
+       :console.log ('no , you dont');
+    alert ('no ,I dont');
+    break;
     
-//     default: 
-//       alert('try agin');
+    default: 
+      alert('try agin');
       
-<<<<<<< HEAD
-// }
-
-
-// let s2=prompt ('do I speak spanish ?');
-// switch(s2.toUpperCase()){
-//     case 'YES' 
-//        :console.log ('yes , you do');
-//     case 'Y'
-//        :console.log ('yes , you do');
-//    alert('yes , I do');
-//     score++;
-//     break;
-=======
 }
 }
 question1();
 
 
-function question2(){
+function question2 (){
 let s2=prompt ('do I speak spanish ?');
 switch(s2.toUpperCase()){
-    case 'YES' :console.log ('yes , you do');
-    case 'Y':console.log ('yes , you do');
+    case 'YES' 
+       :console.log ('yes , you do');
+    case 'Y'
+       :console.log ('yes , you do');
    alert('yes , I do');
+    score++;
     break;
->>>>>>> 2d45c854b6a1847d4764f80c9ee25938de9f50c0
     
-//     case'NO'
-//        :console.log('no , you dont');
-//     case 'N'
-//        :console.log ('no , you dont');
-//    alert ('no ,I dont');
-//     break;
+    case'NO'
+       :console.log('no , you dont');
+    case 'N'
+       :console.log ('no , you dont');
+   alert ('no ,I dont');
+    break;
     
-//     default: 
-//       alert('try agin');
+    default: 
+      alert('try agin');
       
-<<<<<<< HEAD
-// }
-
-
-// let s3=prompt ('AM I traind as carpentry?');
-// switch(s3.toUpperCase()){
-//     case 'YES' 
-//         :console.log ('yes , you do');
-//     case 'Y'
-//        :console.log ('yes , you do');
-=======
 }
 }
 question2();
 
-function question3(){
+function question3 (){
 let s3=prompt ('AM I traind as carpentry?');
 switch(s3.toUpperCase()){
-    case 'YES' :console.log ('yes , you do');
-    case 'Y':console.log ('yes , you do');
->>>>>>> 2d45c854b6a1847d4764f80c9ee25938de9f50c0
+    case 'YES' 
+        :console.log ('yes , you do');
+    case 'Y'
+       :console.log ('yes , you do');
     
-//     alert('yes , I do');
-//     score++;
-//     break;
+    alert('yes , I do');
+    score++;
+    break;
     
-//     case'NO':console.log('no , you dont');
-//     case 'N':console.log ('no , you dont');
-//     alert ('no ,I dont');
-//     break;
+    case'NO':console.log('no , you dont');
+    case 'N':console.log ('no , you dont');
+    alert ('no ,I dont');
+    break;
     
-//     default: 
-//       alert('try agin');
+    default: 
+      alert('try agin');
       
-<<<<<<< HEAD
-// }
-
-
-// let s4=prompt ('AM I good at crafts ?');
-// switch(s4.toUpperCase()){
-//     case 'YES' 
-//          :console.log ('yes , you do');
-//     case 'Y'
-//         :console.log ('yes , you do');
-//    alert('yes , I do');
-//    score++;
-//     break;
-=======
 }
 }
 question3();
 
 
-
 question4();
+function question4 (){
 
-function question4(){
 let s4=prompt ('AM I good at crafts ?');
 switch(s4.toUpperCase()){
-    case 'YES' :console.log ('yes , you do');
-    case 'Y':console.log ('yes , you do');
+    case 'YES' 
+         :console.log ('yes , you do');
+    case 'Y'
+        :console.log ('yes , you do');
    alert('yes , I do');
+   score++;
     break;
->>>>>>> 2d45c854b6a1847d4764f80c9ee25938de9f50c0
     
-//     case'NO'
-//         :console.log('no , you dont');
-//     case 'N'
-//        :console.log ('no , you dont');
-//     alert ('no ,I dont');
-//     break;
+    case'NO'
+        :console.log('no , you dont');
+    case 'N'
+       :console.log ('no , you dont');
+    alert ('no ,I dont');
+    break;
     
-//     default: 
+    default: 
       
-<<<<<<< HEAD
-//       alert('try agin');
+      alert('try agin');
       
-// }
-
-
-// let s5=prompt ('did i study at aabu unversity ?');
-// switch(s5.toUpperCase()){
-//     case 'YES' 
-//         :console.log ('yes , you do');
-//     case 'Y'
-//        :console.log ('yes , you do');
-//    alert('yes , I do');
-//    score++;
-//     break;
-=======
 }
 }
+
 
 question5();
-function question5(){
+function question5 (){
 let s5=prompt ('did i study at aabu unversity ?');
 switch(s5.toUpperCase()){
-    case 'YES' :console.log ('yes , you do');
-    case 'Y':console.log ('yes , you do');
+    case 'YES' 
+        :console.log ('yes , you do');
+    case 'Y'
+       :console.log ('yes , you do');
    alert('yes , I do');
+   score++;
     break;
->>>>>>> 2d45c854b6a1847d4764f80c9ee25938de9f50c0
     
-//     case'NO'
-//         :console.log('no , you dont');
-//     case 'N'
-//         :console.log ('no , you dont');
-//     alert ('no ,I dont');
-//     score++;
-//     break;
+    case'NO'
+        :console.log('no , you dont');
+    case 'N'
+        :console.log ('no , you dont');
+    alert ('no ,I dont');
+    score++;
+    break;
     
-//     default: 
-//       alert('try agin'); 
+    default: 
+      alert('try agin'); 
       
-// }
 
-// alert ( userName +  '  lets play together  guessing game  ');
-
-// let s6 =0;
-// for (let i = 1; i < 4 ; i++) {
-//   let answer = Number(prompt ('can you guess how old i am ?'));
-//   if (answer === 24)
-//   {
-//      alert ('you answerd correctly ' );
-//      score++; 
-//      break;
-//   }
-//   else if (answer < 24 ){
-//  alert('no , it is too low');
-//  s6++;
-//   }
-//   else if (answer > 24){
-//     alert('no , it is too high');
-//     s6++
-//   }
-// }
+    }
+  }
 
 
-// if (ansnwer1==4){
-//   alert (' your attempts have ended , the correct answer is 24') ;
-// }
 
+alert ( userName +  '  lets play together  guessing game  ');
+
+function question6 (){
+
+let s6 =0;
+for (let i = 1; i < 4 ; i++) {
+  let answer = Number(prompt ('can you guess how old i am ?'));
+  if (answer === 24)
+  {
+     alert ('you answerd correctly ' );
+     score++; 
+     break;
+  }
+  else if (answer < 24 ){
+ alert('no , it is too low');
+ s6++;
+  }
+  else if (answer > 24){
+    alert('no , it is too high');
+    s6++
+  }
+}
+
+
+if (ansnwer==4){
+  alert (' your attempts have ended , the correct answer is 24') ;
+}
+}
+question6();
+
+
+function question7 (){
 let favMovie = ['saw', 'it', 'call', 'final destination' ];
 
 let s7 = 0 ;
@@ -237,64 +186,13 @@ m1=m1.toUpperCase();
 s7++;
 } 
 }
-<<<<<<< HEAD
+}
+question7();
    
     
 if (s7==6){alert (' Your chances of guessing have expired , my favourite foods are : ' + favMovie ) ;}
-=======
-}
-
-function question6()
-{let answer = prompt ('guess how old i am', 'answer with numbers' );
-
-console.log(answer);
-for (let i = 1; i < 4 ; i++) {
-if (answer  === '24'){
-  alert('you answerd correctly ');
-  score++;
-  alert ('your score' + score );
-break;
-} else if (answer > 24) {
-  alert('no , it is too high');
-  answer=  prompt ('guess again' , 'answer with numbers' );
-   
-} else if (answer < 24){
-  alert('no , it is too low');
-  answer=  prompt ('guess again' , 'answer with numbers' );
- alert ('you finish your attempt my age is 24 ')
-
-}
-}
-}
-question6();
 
 
-
-function question7() {
-var favMovie = ['it', 'saw', 'call', 'final  destination'];
-
-for (var i = 0; i < 6; i++) {
-var answerd = prompt('Can you guess my fav horror movie ?');
- var guess = answerd.toLowerCase();
-
-for (var j = 0; j < favMovie.length; j++){
-if (guess === favMovie[j]) {
-alert('you answerd correctly , good job');
-score++;
-break;
-}
-} if (i !==6){
-alert('Sorry wrong answer, try again');
-}
-
-alert('you finish your attempt my,the right answer is: the call- saw- final destination -it')
->>>>>>> 2d45c854b6a1847d4764f80c9ee25938de9f50c0
-
-
-<<<<<<< HEAD
 alert ('your score is ' +  score + ' great job '+ userName );
-=======
-question7();
->>>>>>> 2d45c854b6a1847d4764f80c9ee25938de9f50c0
 
 alert ('Thank you  ' +  userName  +  'have a nice day ' );
